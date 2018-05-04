6a5ef253fe9b41ba31b4d84839cf8dee793149c5
bdito <m.moldaschl@adito.de>
2018-05-04 17:53:20 +0200

    fixed ssl connector port with self signed certificate

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sed -i '84s/<!--/ /' /usr/local/tomcat/conf/server.xml
-sed -i '88s/-->/ /' /usr/local/tomcat/conf/server.xml
+sed -i '87s/<!--/ /' /usr/local/tomcat/conf/server.xml
+sed -i '95s/-->/ /' /usr/local/tomcat/conf/server.xml
 
-$JAVA_HOME/bin/keytool -genkey -alias tomcat -dname "cn=ADITO, ou=ADITO, o=ADITO, c=DE" -keyalg RSA -storepass changeit -keypass changeit
+$JAVA_HOME/bin/keytool -genkey -alias tomcat -dname "cn=ADITO, ou=ADITO, o=ADITO, c=DE" -keyalg RSA -storepass changeit -keypass changeit -keystore /usr/local/tomcat/conf/localhost-rsa.jks -storetype pkcs12
 
 echo "apiurl = ${APIURL}" > /usr/local/tomcat/webapps/aditomobile/WEB-INF/classes/adito.prop
 echo "apiuser = ${APIUSER}" >> /usr/local/tomcat/webapps/aditomobile/WEB-INF/classes/adito.prop
